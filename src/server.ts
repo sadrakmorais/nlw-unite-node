e71cbdb7b67d250d9ff425c7d35083d8a773364e
Sadrak Morais <sadraksdk1@gmail.com>
2024-04-03 15:29:04 -0300

    feat: 🛠  Add route to post new event

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import fastify from "fastify";
+import z from "zod";
+import { PrismaClient } from "@prisma/client";
+
+const app = fastify();
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+app.get("/", () => {
+  return "Hello World!";
+});
+
+app.post("/events", async (req, res) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  });
+
+  const data = createEventSchema.parse(req.body);
+
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      slug: new Date().toISOString(),
+      maximumAttendees: data.maximumAttendees,
+    },
+  });
+
+  return res.status(201).send({ eventId: event.id });
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP serve running!");
+});
